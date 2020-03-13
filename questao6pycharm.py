63cd169b110ead50c345a576ac2871f040cb4b5c
Marllonviny <marllonlucena@ads.fiponline.edu.br>
2020-03-13 19:25:25 -0300

    Questao 6 referente ao comprimento individual de uma casa

---
+++ questao6pycharm.py
@@ -0,0 +1,17 @@
+comodo1larg = int(input('informe a largura do comodo 1: '))
+comodo1comp = int(input('Informe o comprimento do comodo 1: '))
+comodo2larg = int(input('Informe a largura do comodo 2: '))
+comodo2comp = int(input('Informe o comprimeiro do comodo 2: '))
+comodo3larg = int(input('informe a largura do comodo 3: '))
+comodo3comp = int(input('informe a comprimento do comodo 3: '))
+comodo4larg = int(input('informe a largura do comodo 4: '))
+comodo4comp = int(input('informe a comprimento do comodo 4: '))
+
+areaComodo1 = comodo1comp * comodo1larg
+areaComodo2 = comodo2comp * comodo2larg
+areaComodo3 = comodo3comp * comodo3larg
+areaComodo4 = comodo4comp * comodo4larg
+
+areaTotal = areaComodo1 + areaComodo2 + areaComodo3 + areaComodo4
+
+print(areaTotal)
