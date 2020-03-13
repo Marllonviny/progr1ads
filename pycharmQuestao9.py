8a904aef1b7dfb0ab00badf16aa1d5b2a53c30f7
Marllonviny <marllonlucena@ads.fiponline.edu.br>
2020-03-13 19:36:19 -0300

    questao 9 equivalente as polegadas

---
+++ pycharmQuestao9.py
@@ -0,0 +1,3 @@
+valorPol = float(input('Indique o valor em polegadas: '))
+centimentros = valorPol * 2.54
+print('Os seus dedões com essa quantidade de %.2f equivalem a %.f centímetros.' % (valorPol, centimentros))
