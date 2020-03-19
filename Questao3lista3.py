55109aecc78309446480cc2ee9c2793a6a834524
Marllonviny <marllonlucena@ads.fiponline.edu.br>
2020-03-19 11:44:47 -0300

    Questao 3 da lista 3 finalmente saiu

---
+++ Questao3lista3.py
@@ -0,0 +1,8 @@
+genero = input('Qual é o seu sexo?(F para Feminino e M para masculino): ' )
+
+if genero == 'F':
+    print('Você é uma mulher!')
+elif genero == 'M':
+    print('Você é macho!')
+else:
+    print('Sexo Indefinido')
