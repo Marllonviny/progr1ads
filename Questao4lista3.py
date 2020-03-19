d799d1e27f9529a00d2e32c87627f81a7b0b2288
Marllonviny <marllonlucena@ads.fiponline.edu.br>
2020-03-19 10:47:38 -0300

    quarta questão da terceira lista do curso de ads

---
+++ Questao4lista3.py
@@ -0,0 +1,8 @@
+letras = input('Digite uma letra por favor: ')
+
+vogal = ('a','e','i','o','u','A','E','I','O','U')
+
+if letras in vogal:
+    print('É uma vogal, chapa!')
+else:
+    print('É uma consoante :/')
