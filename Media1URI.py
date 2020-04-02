a4e9ed00e44ab257af3da8b31959d0d16cabc57c
Marllonviny <marllonlucena@ads.fiponline.edu.br>
2020-04-02 18:20:26 -0300

    Questao Media 1 do URi

---
+++ Media1URI.py
@@ -0,0 +1,10 @@
+a = float(input(''))
+b = float(input(''))
+x = a * 3.5
+y = b * 7.5
+media = (x + y) / 11
+media2 = 10
+if media <= 10:
+    print('MEDIA = %.5f' % media)
+else:
+    print('MEDIA = %.5f' % media2)
