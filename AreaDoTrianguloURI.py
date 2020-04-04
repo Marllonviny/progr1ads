e4dd84096dae5eabec97a7ddd95e6eaa757f347c
Marllonviny <marllonlucena@ads.fiponline.edu.br>
2020-04-03 21:54:23 -0300

    Questão da area do URI

---
+++ AreaDoTrianguloURI.py
@@ -0,0 +1,19 @@
+a, b, c = input().split()
+
+a = float(a)
+b = float(b)
+c = float(c)
+
+pi = 3.14159
+
+areaTriangulo = (a * c) / 2
+areaCirculo = pi * c**2
+areaTrapezio = ((a * b) * c) / 2
+areaQuadrado = b**2
+arearetangulo = a * b
+
+print('TRIANGULO: %.3f' % areaTriangulo)
+print('CIRCULO: %.3f' % areaCirculo)
+print('TRAPEZIO: %.3f' % areaTrapezio)
+print('QUADRADO: %.3f' % areaQuadrado)
+print('RETANGULO: %.3f' % arearetangulo)
