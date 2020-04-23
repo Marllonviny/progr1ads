8700839421685317b171248673c4ad2de2911fce
Marllonviny <61851739+Marllonviny@users.noreply.github.com>
2020-04-23 14:26:43 -0300

    Questao 10 lista 5

---
+++ recepcaodenumeros.py
@@ -0,0 +1,9 @@
+numero1 = int(input('Digite o primeiro numero: '))
+numero2 = int(input('Digite o segundo numero: '))
+contador = numero1 + 1
+
+while contador < numero2:
+    print(contador)
+    contador += 1
+
+
