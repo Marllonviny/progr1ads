8478e8099971a475eb7b9cc67705c14c11d31535
Marllonviny <61851739+Marllonviny@users.noreply.github.com>
2020-04-23 14:20:17 -0300

    questao 4 lista 5

---
+++ taxadenatalidade.py
@@ -0,0 +1,14 @@
+paisA = 80000
+paisB = 200000
+ano = 0
+while paisA < paisB:
+    paisA = paisA * 1.03
+    paisB = paisB * 1.015
+    ano = ano + 1
+    print(ano)
+print('Passaram %s anos' % ano)
+
+
+
+
+
